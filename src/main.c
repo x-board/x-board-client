21cb50cbadab83dfe2286072ab758a26dff64269
Jasper Horn <jasperhorn@gmail.com>
2017-02-13 11:01:12 +0100

    [#16] Create client that supports digital write
    On all pins and in a syntax that I think is going to stay.

--- src/main.c
+++ src/main.c
@@ -1,20 +1,73 @@
 
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char** argv)
 {
-    if (argc != 2 || (strcmp(argv[1], "HIGH") != 0 && strcmp(argv[1], "LOW") != 0))
+    if (argc < 2)
     {
-        printf("Failed\n");
-        printf("argc: %d\n", argc);
+        printf("No mode or operation specified\n");
+        return -1;
+    }
+
+    uint16_t message[] = {36<<1, 0, 0, 0, 0};
+    int messageLength = 0;
+
+    if (strcmp(argv[1], "set") == 0)
+    {
+        message[1] = 0x01;
+
+        if (argc < 4)
+        {
+            printf("Not enough arguments for 'set'\n");
+            return -1;
+        }
+
+        if (strncmp(argv[2], "pin:", 2) == 0)
+        {
+            long pin = strtol(argv[2] + 4, 0, 10);
+
+            if (pin != 1 && (pin < 3 || pin > 5))
+            {
+                printf("Invalid pin number for 'set'\n");
+                return -1;
+            }
+
+            message[2] = 0x01;
+            message[3] = pin;
+
+            if (strcmp(argv[3], "high") == 0)
+            {
+                message[4] = 1;
+                messageLength = 5;
+            }
+            else if (strcmp(argv[3], "low") == 0)
+            {
+                message[4] = 0;
+                messageLength = 5;
+            }
+            else
+            {
+                printf("Invalid value for 'set <pin>': %s\n", argv[3]);
+                return -1;
+            }
+        }
+        else
+        {
+            printf("Unexpected argument for 'set'\n");
+            return -1;
+        }
+    }
+    else
+    {
+        printf("Unknown mode: %s\n", argv[1]);
         return -1;
     }
 
     int i2c_handle = i2c_open(2);
-    uint16_t msg[] = {36<<1, 0x01, 0x01, 0x01, strcmp(argv[1], "LOW") == 0 ? 0x00 : 0x01};
 
-    i2c_send_sequence(i2c_handle, msg, 5, 0);
+    i2c_send_sequence(i2c_handle, message, messageLength, 0);
 
     return 0;
 }

54073e117c8cb18a2d4953358d7b23083ef8a701
Jasper Horn <jasperhorn@gmail.com>
2017-12-19 17:40:21 +0100

    [#67] Use C++ vector, preventing size mistakes

--- src/x-board.cpp
+++ src/x-board.cpp
@@ -1,6 +1,8 @@
 
 #include "x-board.hpp"
 
+#include <vector>
+
 extern "C"
 {
   #include "../lsquaredc/lsquaredc.h"
@@ -17,25 +19,25 @@ extern "C"
 #define XB_VAL_DIGITAL_LOW 0
 
 
-void sendMessage(uint16_t* message, int size)
+void sendMessage(std::vector<uint16_t>  message)
 {
     int i2c_handle = i2c_open(2);
 
-    i2c_send_sequence(i2c_handle, message, size, 0);
+    i2c_send_sequence(i2c_handle, message.data(), message.size(), 0);
 
     i2c_close(i2c_handle);
 }
 
 void xboardSetDigital(uint8_t pin, bool on)
 {
-    uint16_t message[] = {36<<1, XB_MODE_SET, XB_OP_SET_DIGITAL, pin, on ? XB_VAL_DIGITAL_HIGH : XB_VAL_DIGITAL_LOW};
+    std::vector<uint16_t> message = {36<<1, XB_MODE_SET, XB_OP_SET_DIGITAL, pin, on ? XB_VAL_DIGITAL_HIGH : XB_VAL_DIGITAL_LOW};
 
-    sendMessage(message, 5);
+    sendMessage(message);
 }
 
 void xboardSetPWM(uint8_t pin, uint8_t value)
 {
-    uint16_t message[] = {36<<1, XB_MODE_SET, XB_OP_SET_ANALOG, XB_ANALOG_MODE_PWM, pin, value};
+    std::vector<uint16_t> message = {36<<1, XB_MODE_SET, XB_OP_SET_ANALOG, XB_ANALOG_MODE_PWM, pin, value};
 
-    sendMessage(message, 6);
+    sendMessage(message);
 }
